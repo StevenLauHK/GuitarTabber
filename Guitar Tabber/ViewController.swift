2bc9b2652244c55863669272d7c6ee37392f32f7
Lau Kin Wai <laukinwai@MacBook-Pro-2.local>
2016-10-07 17:45:17 +0800

    First commit

---
+++ Guitar Tabber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Guitar Tabber
+//
+//  Created by Lau Kin Wai on 7/10/2016.
+//  Copyright © 2016年 EE4304. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
