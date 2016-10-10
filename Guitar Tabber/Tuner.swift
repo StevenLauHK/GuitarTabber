4f23772b4004acafd573c8686a9eae8060fa76af
enriquemc96 <enriquemc96@gmail.com>
2016-10-10 12:31:53 +0800

    Tuner added

--- Guitar Tabber/Tuner.swift
+++ Guitar Tabber/Tuner.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import TuningFork
+
+class GuitarTuner: TunerDelegate {
+    private var tuner: Tuner
+
+    private var pitch: String
+    private var octave: Int
+    public private(set) var distance: Float
+
+    public var note: String { get { return pitch + String(octave) } }
+
+    init() {
+        tuner = Tuner()
+        tuner.delegate = self
+        tuner.start()
+    }
+
+    private func tunerDidUpdate(tuner: Tuner, output: TunerOutput) {
+        pitch = output.pitch
+        octave = output.octave
+        distance = output.distance
+    }
+}
