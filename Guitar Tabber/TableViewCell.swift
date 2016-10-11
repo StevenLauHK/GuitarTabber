c10c0b5940d51cf04865590a5967ce6b09f6c327
StevenLauHK <laukinwaisteven@gmail.com>
2016-10-11 12:27:04 +0800

    UI Change

---
+++ Guitar Tabber/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Guitar Tabber
+//
+//  Created by Lau Kin Wai on 10/10/2016.
+//  Copyright © 2016年 EE4304. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
